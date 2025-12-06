0ae6407b407804d46da99df4db5c6d3422d0d1e3
Kelton <kelton.jesus23@gmail.com>
2025-12-06 19:01:28 +0000

    Minor

--- lib/features/item_details/item_details_screen.dart
+++ lib/features/item_details/item_details_screen.dart
@@ -98,7 +98,7 @@ class ItemDetailsScreen extends StatelessWidget {
                       child: Icon(
                         item.icon,
                         size: 100,
-                        color: Colors.white.withOpacity(0.9),
+                        color: Colors.white.withValues(alpha: 0.9),
                       ),
                     ),
                   ),
@@ -236,7 +236,7 @@ class ItemDetailsScreen extends StatelessWidget {
               color: Colors.white,
               boxShadow: [
                 BoxShadow(
-                  color: Colors.black.withOpacity(0.05),
+                  color: Colors.black.withValues(alpha: 0.05),
                   blurRadius: 10,
                   offset: const Offset(0, -2),
                 ),
